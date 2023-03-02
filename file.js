1cbcc931c700c7ed1bba640f889e9cbaf9ba9e0e
mrugank1 <bal.mrugank@gmail.com>
2023-03-02 13:32:41 +0530

    ...

---
+++ file.js
@@ -0,0 +1,5 @@
+carName = "Saab";
+let carName = "Volvo";
+console.log(carName)
+
+
---
+++ test.js
@@ -0,0 +1,59 @@
+      //let age =20
+     //let firstName = "harsit";
+
+
+
+     //age = age + "";                       //no. to string //
+     //console.log(typeof (age)); "20"
+
+
+       // let myStr = +'34'                 //string to no//
+       //console.log(typeof myStr);
+
+
+// let age = 18;
+// age = String(age);
+// console.log(typeof age)
+
+//let variable=100
+//console.log(variable)
+
+
+
+
+
+//javascript can change HTML Content
+//document.getElementById("1").innerHTML = "Hello JavaScript";
+
+
+
+
+//javascript can change HTML attribute values
+//document.getElementById("demo").innerHTML = 
+//javascrpit can can the source attribute of an image
+
+
+
+
+//javascript can change HTML Styles(CSS)
+//document.getElementById("demo").style.fontSize = "35px"
+
+
+
+
+
+
+
+
+
+
+// javascript can hide HTML Elements
+//document.getElementById("demo").style.display = "none"
+
+
+
+
+//javascript can show HTML Elements
+//document.getElementById("demo").style.display = "block"
+
+
